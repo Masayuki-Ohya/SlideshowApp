d59171651edd77d9164aad8848f4b38ed58857d4
Masayuki-Ohya <oymsyk@gmail.com>
2020-04-01 21:53:34 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by 大矢政行 on 2020/04/01.
+//  Copyright © 2020 masayuki.ohya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
